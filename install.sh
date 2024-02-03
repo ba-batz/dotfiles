7cfc6dfe7fe7517527ab008f2f9b8b9379b07c03
ba-batz <batz.cool@proton.me>
2024-02-03 12:23:45 +0100

    add installer

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ ! -d "~/.var/" ]
+then
+   mkdir ~/.var/
+fi
+  
+if [ ! -d "~/.var/dotfile/" ]
+then
+   mkdir ~/.var/dotfile/
+fi
+
+cp -r ./* ~/.var/dotfile/
+
+chmod a+x ~/.var/dotfile/home/.local/bin/*
+
+exec ~/.var/dotfile/home/.local/bin/dotapply
+exec ~/.var/dotfile/home/.local/bin/etc-dotapply
