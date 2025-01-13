843ca5609655614f0da9751bec85e916e1a65a18
ba-batz <batz.cool@proton.me>
2025-01-13 21:05:59 +0100

    lulzU

--- user/dot-config/fish/config.fish
+++ user/dot-config/fish/config.fish
@@ -1,6 +1,6 @@
 set -U fish_greeting
 set -U fish_user_paths $HOME/.local/bin
-abbr mew 'kitty +kitten ssh admin@192.168.1.2'
+abbr mew 'kitty +kitten ssh admin@192.168.1.9'
 abbr wol 'for i in (seq 200); wol --ipaddr=192.168.1.2 --port=9 --wait=3000 40:61:86:01:bc:38; end'
 abbr vps 'kitty +kitten ssh batz@85.215.147.32'
 abbr dit 'cd ~/.var/dotfile'
